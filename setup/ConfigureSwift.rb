753adb0ef23f936fd9efad6e0b0994608c75773a
pq <devpgq@126.com>
2020-04-14 15:16:56 +0800

    取消ReplaceMe文件的创建

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -52,7 +52,7 @@ module Pod
 
       # There has to be a single file in the Classes dir
       # or a framework won't be created
-      `touch Pod/Classes/ReplaceMe.swift`
+      # `touch Pod/Classes/ReplaceMe.swift`
       
       `mkdir Pod/Classes/Controller`
       `mkdir Pod/Classes/View`
